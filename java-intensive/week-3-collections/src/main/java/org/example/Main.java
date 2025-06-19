b639d21246ba08735140f152704b481fbeeff9e0
madinabolat <madinabolat01@gmail.com>
2025-06-18 22:00:18 -0400

    Update Main.java

--- java-intensive/week-3-collections/src/main/java/org/example/Main.java
+++ java-intensive/week-3-collections/src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+
     }
 }
