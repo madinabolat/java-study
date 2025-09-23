441ad4faf2593716afa259ac58cc3b955f6ef99c
madinabolat <madinabolat01@gmail.com>
2025-09-22 21:53:10 -0400

    config: hw 3 part 1

--- java-intensive/week-3-collections/src/main/java/org/example/Contact.java
+++ java-intensive/week-3-collections/src/main/java/org/example/Contact.java
@@ -2,21 +2,17 @@ package org.example;
 
 public class Contact {
     public String name;
-    public String phoneNumber;
-    public String email;
+    private String phoneNumber;
+    private String email;
 
-    Contact(String name, String phoneNumber, String email){
+    public Contact(String name, String phoneNumber, String email){
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
 
     @Override
-    public String toString() {
-        return "Contact{" +
-                "name='" + name + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", email='" + email + '\'' +
-                '}';
+    public String toString(){
+        return "Contact{name="+name+", phone = "+phoneNumber+", email = "+email+"}";
     }
 }
--- java-intensive/week-3-collections/src/main/java/org/example/Main.java
+++ java-intensive/week-3-collections/src/main/java/org/example/Main.java
@@ -1,26 +1,7 @@
 package org.example;
 
-import java.util.ArrayList;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Contact contact1 = new Contact("Madina", "217-904-96-27", "madina@google.com");
-        Contact contact2 = new Contact("Edward", "111-111-11-11", "edward@boston.com");
-
-        ContactManager contacts = new ContactManager();
-        contacts.addContact(contact1);
-        contacts.addContact(contact2);
-        contacts.removeContact(contact2);
-
-        contacts.displayAllContacts();
-
-        contacts.lookupPhoneNumber("Madina");
-        contacts.updatePhoneNumber("Madina","111-111-11-11");
-        System.out.println("Check");
-        contacts.displayAllContacts();
-
 
     }
 }
