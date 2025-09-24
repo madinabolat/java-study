ebf30686c0925ed8aa16529e4c51b7421953abe3
madinabolat <madinabolat01@gmail.com>
2025-09-23 22:52:09 -0400

    Update Main.java

--- java-intensive/week-3-collections/src/main/java/org/example/Main.java
+++ java-intensive/week-3-collections/src/main/java/org/example/Main.java
@@ -5,6 +5,13 @@ public class Main {
         Contact contact1 = new Contact("Madina", "123", "m@gmail.com");
         Contact contact2 = new Contact("Edward", "123", "e@gmail.com");
         Contact contact3 = new Contact("Madina", "456", "m2@gmail.com");
+        ContactManager contactManager = new ContactManager();
+        contactManager.addContact(contact1);
+        contactManager.addContact(contact2);
+        contactManager.addContact(contact3);
+        contactManager.displayAllContacts();
+        contactManager.lookupPhoneNumber("Madina");
+        contactManager.lookupPhoneNumber("Peach");
 
     }
 }
